bfa52f8f7fcb601672dabba78aac5159bebe97d6
MichalTzadok <michal9741003@gmail.com>
2024-05-06 17:49:07 +0300

    beginning of the project includes the opening of folders

---
+++ app/routes/user_router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.db_management import user_CRUD
+
+user_router = APIRouter()
+
+@user_router.get('/all')
+async def get_all_users():
+    return await user_CRUD.get_all_users()
