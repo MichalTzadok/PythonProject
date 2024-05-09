afaefeee94daa5708faac0c7287b42c8ae4b0ee6
MichalTzadok <michal9741003@gmail.com>
2024-05-09 15:58:19 +0300

    add user_budget

--- app/models/user_budget.py
+++ app/models/user_budget.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 class User_budget(BaseModel):
+     id:int
      user_id:str
      expenses:int
      revenues:int
+     date_time:datetime
--- app/services/db.py
+++ app/services/db.py
@@ -2,5 +2,6 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 
-db = client['expenses&Income']
-users = db['expenses&Income']
+db = client['Finance_Focus']
+users = db['Users']
+users_budgets = db['Users_Budgets']
