ae9595d907f8e888c3a6fc5ea1501b5ee5ac81f1
MichalTzadok <michal9741003@gmail.com>
2024-05-24 02:24:48 +0300

    Small correction, validations and maintaining the order of the tests and editing the readme file

--- tests/test_user_action.py
+++ tests/test_user_action.py
@@ -6,34 +6,39 @@ from app.services import user_action_service
 """This module defines unit tests for user action-related functionalities in the app."""
 
 @pytest.mark.asyncio
+@pytest.mark.order(1)
 async def test_create_user_action():
     """Tests the creation of a user action."""
     new_user_action = User_Action(id=1, user_id=1, type='expense', amount=100, datetime=datetime.now())
     result = await user_action_service.Create_user_action(new_user_action)
-    assert result.id == new_user_action.id
+    assert result.id is not None
+    assert result.user_id == new_user_action.user_id
     assert result.type == new_user_action.type
     assert result.amount == new_user_action.amount
     assert result.datetime == new_user_action.datetime
 @pytest.mark.asyncio
+@pytest.mark.order(3)
 async def test_update_user_action():
     """Tests the update of a user action."""
-    user_action = User_Action(id=2, user_id=1, type='expense', amount=200, datetime=datetime.now())
+    user_action = User_Action(id=15, user_id=1, type='expense', amount=200, datetime=datetime.now())
     result = await user_action_service.update_user_action(user_action)
     assert result == user_action
 
 @pytest.mark.asyncio
+@pytest.mark.order(4)
 async def test_delete_user_action():
     """Tests the deletion of a user action."""
-    user_action_id = 2
+    user_action_id = 10
     result = await user_action_service.delete_user_action(user_action_id)
     assert result is None
 
 
 @pytest.mark.asyncio
+@pytest.mark.order(2)
 async def test_get_user_action_by_user_id():
     """Tests the retrieval of a user action by user ID."""
     user_id = 1
-    user_action_id = 2
+    user_action_id =8
     user_action = await user_action_service.get_user_action_by_user_id(user_id, user_action_id)
     assert user_action.id == user_action_id
     assert user_action.user_id == user_id
